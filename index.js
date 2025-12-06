2362ab1fc92b67ba6ced970c2fc751f2c2f22a0e
himanshu prajapat <salasarhimanshu6@gmail.com>
2025-12-06 06:31:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+// // for(let i = 1; i <= 3; i++){
+// //     for(let j = 1; j <= 3; j++){
+// //         console.log(i, j);
+// //     }
+// // }
+
+
+
+// // question 1
+// // let row = ""
+// // for(let i = 1; i<=5;i++){
+// //  let row = "";
+// //  for(let j = 1; j<=5;j++){
+// //     row = row + "*"
+// //  }   
+// //   console.log(row)
+
+// // }
+
+
+// // question 2
+// // let row = ""
+// // for(let i = 1;i<=5; i++){
+// //     row = row + "*";
+// // console.log(row)
+// // }
+
+// // let row = "";
+// // for(let i = 5; i>1; i--){
+// //     row=row+""
+// //     console.log(row)
+// // }
+
+
+// // for(let i = 5; i>=1; i--){
+// //     let row = ""
+// //     for(let j=1; j<=i;j++){
+// //         row+="*"
+// //     }
+// //     console.log(row)
+// // }
+
+// let row = ""
+// for(let i =1; i<=5; i++){
+//     row = row + i
+//     console.log(row)
+// }
+
+// for(let i = 1; i <= 3; i++){
+// let a = ""
+// for(let j = 1; j <= 3; j++){
+//     a=a+i
+// }
+// console.log(a)
+// }
+// for(let i = 1; i<=5; i++){
+//     let row = "";
+//     for(let j =1; j<=5; j++){
+//         row=row+"*"
+//     }
+//     console.log(row)
+// }
+
+// let a = "";
+//  for(let i =1; i<=5; i++){
+// a = a+"*"
+// console.log(a)
+//  }
+
+
+
+//  for(let i = 5; i>=1; i--){
+//     // a = a+"*"
+//  let b = "";
+//     for(let j = 1; j<=i;j++){
+//         b+="*"
+//     }
+//     console.log(b)
+//  }
+
+
+//  let c ="";
+//  for(let i = 1;i<=5;i++){
+//     c=c+i
+//     console.log(c)
+//  }
+
+
+//  for(let i = 1; i<=3; i++){
+//     let row = ""
+//     for(let j = i; j<=i+2;j++){
+//         row+= j + " "
+//     }
+//     console.log(row)
+//  }
+
+
